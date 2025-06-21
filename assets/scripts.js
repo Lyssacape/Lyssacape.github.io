84033627e0ee8e2ae21f7b2edaf555fef2de17c5
Lyssa <Alynicc123@gmail.com>
2025-06-21 12:31:27 -0400

    Initial PowerShell CLI layout

---
+++ assets/scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const output = document.getElementById('output');
+  output.innerHTML = `<p>Type <code>show-projects</code> or <code>get-help</code></p>`;
+});
